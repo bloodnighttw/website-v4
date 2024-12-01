4c23fab548311cf88c54795e7d12e3613305733c
bloodnighttw <emails@bntw.dev>
2024-12-02 00:54:19 +0800

    fix some ui error.

--- app/page.tsx
+++ app/page.tsx
@@ -10,33 +10,34 @@ export default function Home() {
 			initial={{opacity: 0.01}}
 			animate={{opacity: 1}}
 			exit={{opacity: 0.01}}
-			className="introduce-content"
+			className="introduce"
 		>
+			<div className="flex flex-col sm:flex-row-reverse gap-x-12 gap-y-4 sm:items-start items-center">
+				<Image
+					alt="View bloodnighttw's full-sized avatar"
+					src="https://avatars.githubusercontent.com/u/44264182?s=460&u=b59e580f37ab7e6a3979ab8a6df1f12ba6588069&v=4"
+					className="items-center rounded-full w-28 h-28 sm:w-40 sm:h-40"
+					width={160}
+					height={160}
+					loading="eager"
+				/>
 
-			<Image
-				alt="View bloodnighttw's full-sized avatar"
-				src="https://avatars.githubusercontent.com/u/44264182?s=460&u=b59e580f37ab7e6a3979ab8a6df1f12ba6588069&v=4"
-				className="items-center rounded-full w-28 h-28 sm:w-40 sm:h-40"
-				width={160}
-				height={160}
-				loading="eager"
-			/>
-
-			<div className="introduce-text">
-				<p className="rwd-show">{"👋 hi! I'm"}</p>
-				<p className="rwd-hidden">👋 hi!</p>
-				<h1 className="rwd-hidden">
-					{"I'm"}
-				</h1>
-				<h1>
-					bloodnighttw
-				</h1>
-				<div
-					className="introduce-links">
-					<Link href="/about">about</Link> /
-					<Link href="/projects">projects</Link> /
-					<Link href="/contact">contact</Link> /
-					<Link href="/blog">blog</Link>
+				<div className="introduce-text">
+					<p className="rwd-show">{"👋 hi! I'm"}</p>
+					<p className="rwd-hidden">👋 hi!</p>
+					<h1 className="rwd-hidden">
+						{"I'm"}
+					</h1>
+					<h1>
+						bloodnighttw
+					</h1>
+					<div
+						className="introduce-links">
+						<Link href="/about">about</Link> /
+						<Link href="/projects">projects</Link> /
+						<Link href="/contact">contact</Link> /
+						<Link href="/blog">blog</Link>
+					</div>
 				</div>
 			</div>
 
