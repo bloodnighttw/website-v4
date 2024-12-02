feb960bda257651a0199c0835eceaee5a923cd4a
bloodnighttw <emails@bntw.dev>
2024-12-02 23:25:28 +0800

    defined way to decode markdown to ast.

--- utils/post.ts
+++ utils/post.ts
@@ -1,53 +1,17 @@
-import * as fs from "node:fs";
-import path from "node:path";
-import matter from 'gray-matter';
+import {unified} from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import frontmatter from "remark-frontmatter";
 
-export interface PostMeta {
-	path: string;
-	title: string;
-	date: string;
-}
+// when calling this function, it will return the AST of the markdown file
+// with GFM (Github Flavored Markdown) and frontmatter enabled.
+// The AST contains the metadata and the content of the markdown file, which will be used later.
+// This function is internal and should not be used outside of this module.
+const markdown2astUnified = unified()
+	.use(remarkParse) // Convert markdown into AST
+	.use(remarkGfm) // Enable GFM
+	.use(frontmatter) // Parse frontmatter
+	// You can add more remark plugins here by calling .use(pluginName)
 
-// cache the data, so we don't have to read the file every time
-// if the data has new posts, the server will need to be restarted,
-// so we don't have to worry about the cache being outdated.
-// We might use a database in the future to store the data.
-// For now, this is good enough.
-let dataCache: PostMeta[] | null = null;
-
-// read the file under the posts directory /posts
-export async function getPostMeta() {
-
-	if(dataCache) {
-		return dataCache
-	}
-
-	const posts = await fs.promises.readdir(path.join(process.cwd(), 'posts'));
-
-	const mdxPosts = posts.filter((post) => post.endsWith('.mdx'));
-
-	const postMetas:Array<PostMeta> = [];
-
-	for(const post of mdxPosts) {
-		const postContent = await fs.promises.readFile(path.join(process.cwd(), 'posts', post), 'utf8');
-		const { data } = matter(postContent);
-		postMetas.push({
-			date: "1970-01-01",
-			title: "This article is missing a title",
-			path: post.replace(/\.[^/.]+$/, ""), // remove the file extension
-			...data,
-		});
-	}
-
-	dataCache = postMetas;
-
-	return postMetas;
-}
-
-export async function getPostByPath(name: string) {
-	const postContent = await fs.promises.readFile(path.join(process.cwd(), 'posts', name + '.mdx'), 'utf8');
-	const {content} = matter(postContent);
-	return content;
-}
-
-export default getPostMeta;
+export const markdown2ast =  (content: string) =>
+	markdown2astUnified.parse(content);
