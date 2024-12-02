49e245a3af1d250aaa97990a408d80a8f83a3b67
bloodnighttw <emails@bntw.dev>
2024-12-03 02:17:42 +0800

    change params to filename instead of content.

--- utils/post.ts
+++ utils/post.ts
@@ -2,6 +2,8 @@ import {unified} from "unified";
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import frontmatter from "remark-frontmatter";
+import fs from "node:fs";
+import path from "node:path";
 
 // when calling this function, it will return the AST of the markdown file
 // with GFM (Github Flavored Markdown) and frontmatter enabled.
@@ -13,5 +15,10 @@ const markdown2astUnified = unified()
 	.use(frontmatter) // Parse frontmatter
 	// You can add more remark plugins here by calling .use(pluginName)
 
-export const markdown2ast =  (content: string) =>
-	markdown2astUnified.parse(content);
+// This function will return the AST of the markdown file.
+// you should pass the file name with the extension to this function.
+// This function is exported and can be used in other modules.
+export const markdown2ast = async (file: string) => {
+	const content = await fs.promises.readFile(path.join(process.cwd(), "posts", file), "utf8");
+	return markdown2astUnified.parse(content);
+}
