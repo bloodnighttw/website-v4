e798e2ce90e23e3b875e5c6bd6a62327559dbd42
bloodnighttw <emails@bntw.dev>
2024-12-02 23:45:54 +0800

    New Metadata fetch function.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,15 @@
-import getPostMeta from "@/utils/post";
+import {getAllMetadata} from "@/utils/posts/metadata";
 
 export default async function Blog() {
 
-	const posts = await getPostMeta();
+	const posts = await getAllMetadata();
 
 	return (
 		posts.map((post) => (
 			<div key={post.path}>
 				<h1>{post.title}</h1>
 				<p>{post.date.toString()}</p>
+				<p>{post.summery}</p>
 			</div>
 		))
 	);
