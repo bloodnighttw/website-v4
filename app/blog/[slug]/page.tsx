4445b0093502382e2a0784351e9bdf5f6050bbb8
bloodnighttw <emails@bntw.dev>
2024-12-06 06:51:13 +0800

    fixed width too small when no image or any big width content.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -36,15 +36,12 @@ export default async function Blog({params}: { params: Promise<BlogProps> }) {
 	return (
 		<div className="flex p-4 justify-center gap-4 pb-0 border-gray-50 xl:h-[calc(100vh-0.5em)]">
 			<div
-				className="article "
+				className="article w-full"
 			>
 				<article
-					className="xl:pr-2 xl:min-w-[56rem] lg:min-w-[48rem] md:min-w-[40rem]"
+					className="xl:pr-2 "
 					dangerouslySetInnerHTML={{__html: content}}
 				/>
-				<div className="w-2">
-					123
-				</div>
 			</div>
 			<div
 				className="hidden xl:flex overflow-y-auto no-scrollbar hover:scrollbar h-full w-72  flex-col gap-2 duration-200"
