aa47bcbb5a43885f1237ffedc80884f3ab307247
bloodnighttw <emails@bntw.dev>
2025-01-07 20:01:50 +0800

    fix: forget to fix posts part.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,7 +35,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const allPosts = await getAllMetadata()
 
     const posts:MetadataRoute.Sitemap = allPosts.map((post) => ({
-        url: `/blog/${post.path}`,
+        url: `${BASE_URL}/blog/${post.path}`,
         lastModified: post.date,
         changeFrequency: 'daily',
         priority: 0.7,
