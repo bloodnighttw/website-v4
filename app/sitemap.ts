9b66c86a73e33d512a8898ab69b42fd586f4c84c
bloodnighttw <emails@bntw.dev>
2025-01-07 19:58:15 +0800

    fix: fix url mistake.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,29 +1,31 @@
 import type { MetadataRoute } from 'next'
 import { getAllMetadata } from "@/utils/posts/metadata";
 
+const BASE_URL = 'https://bntw.dev'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     const staticPages:MetadataRoute.Sitemap = [
         {
-            url: '/',
+            url: `${BASE_URL}/`,
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1.0,
         },
+        // {
+        //     url: `${BASE_URL}/about`,
+        //     lastModified: new Date(),
+        //     changeFrequency: 'monthly',
+        //     priority: 0.6,
+        // },
+        // {
+        //     url: `${BASE_URL}/contact`,
+        //     lastModified: new Date(),
+        //     changeFrequency: 'weekly',
+        //     priority: 0.8,
+        // },
         {
-            url: '/about',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.6,
-        },
-        {
-            url: '/contact',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
-        },
-        {
-            url: '/blog',
+            url: `${BASE_URL}/blog`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.9,
