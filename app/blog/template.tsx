efd406190aa9eb14a830e81c3c6be51dbc380e2e
bloodnighttw <emails@bntw.dev>
2025-01-07 16:29:04 +0800

    feat: add new animation and apply to all page.

---
+++ app/blog/template.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { motion } from "motion/react";
+
+const variants = {
+    hidden: { opacity: 0.01, y: 10},
+    enter: { opacity: 1, y:0},
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+
+    return (
+        <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="enter"
+        >
+            {children}
+        </motion.div>
+);
+}
