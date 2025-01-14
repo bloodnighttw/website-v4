184788a39d3aa23f6df9028724925de62c2dff1f
bloodnighttw <bbeenn1227@gmail.com>
2025-01-14 22:22:24 +0800

    featL: unified layout.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ export default function About() {
     return (
         <>
             <div className="about">
+                <div />
                 <Nav title={"about"} />
 
                 <p>
