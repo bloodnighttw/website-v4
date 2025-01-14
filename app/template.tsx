9f093f03c520f2f054f19d98a6b3c7bd878547fe
bloodnighttw <bbeenn1227@gmail.com>
2025-01-15 00:56:11 +0800

    chore: fix back button won't show at first load.

--- app/template.tsx
+++ app/template.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "motion/react";
 import "./globals.css";
+import useBack from "@/utils/back";
 
 const variants = {
     hidden: { opacity: 0.01 },
@@ -13,6 +14,8 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars -- a tricky way to make sure the useEffect under the hood is called
+    const _1 = useBack();
     return (
         <motion.div variants={variants} initial="hidden" animate="show">
             {children}
