db070ff0d526cae24f15942c86461b6bb731e295
bloodnighttw <bbeenn1227@gmail.com>
2025-01-21 04:29:10 +0800

    refactor: yamlParse and test code.

--- tests/mdast2metadata.test.ts
+++ tests/mdast2metadata.test.ts
@@ -1,37 +1,20 @@
-import { describe, it } from "node:test";
 import fs from "node:fs";
 import path from "node:path";
 import { test } from "vitest";
+import { markdown2ast } from "@/utils/blog";
+import yamlParse from "@/utils/posts/mdast2hast";
 
 test("mdast2metadata", async () => {
+    const files = await fs.promises.readdir(path.join(process.cwd(), "posts"));
 
-}
+    const mdFileWithoutExt = files
+        .filter((file) => file.endsWith(".md"))
+        .map((file) => file.replace(/\.md$/, ""));
 
-describe("mdast2metadata", () => {
-    it("list md and show as ast", async () => {
-        const { default: yamlParse } = await import(
-            // @ts-expect-error just ignore it
-            "../utils/posts/mdast2hast.ts"
-        );
-
-        const { default: markdown2ast } = await import(
-            // @ts-expect-error just ignore it
-            "../utils/posts/md2mdast.ts"
-        );
-
-        const files = await fs.promises.readdir(
-            path.join(process.cwd(), "posts"),
-        );
-
-        const mdFileWithoutExt = files
-            .filter((file) => file.endsWith(".md"))
-            .map((file) => file.replace(/\.md$/, ""));
-
-        for (const file of mdFileWithoutExt) {
-            console.log("Reading file:", file);
-            const ast = await markdown2ast(file);
-            const node = yamlParse(ast);
-            console.log(node);
-        }
-    });
+    for (const file of mdFileWithoutExt) {
+        console.log("Reading file:", file);
+        const ast = await markdown2ast(file);
+        const node = yamlParse(ast);
+        console.log(node);
+    }
 });
--- utils/posts/mdast2hast.ts
+++ utils/posts/mdast2hast.ts
@@ -3,4 +3,22 @@
 // It is a tree representation of an HTML.
 // that can be used to manipulate the HTML content programmatically.
 
+// This function will parse the yaml metadata in the markdown file
+import { Root } from "mdast";
+import { find } from "unist-util-find";
+import yaml from "yaml";
 
+interface YAMLNode {
+    type: "yaml";
+    value: string;
+}
+
+export default function yamlParse<T>(ast: Root): T | null {
+    const yamlContent = find(ast, { type: "yaml" }) as YAMLNode;
+
+    if (yamlContent) {
+        return yaml.parse(yamlContent.value) as T;
+    }
+
+    return null;
+}
