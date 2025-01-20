371a36771c622ae2949ba3980d04b70ae752aa0b
bloodnighttw <bbeenn1227@gmail.com>
2025-01-21 03:58:34 +0800

    refactor: switch vitest from mocha

--- tests/md2mdast.test.ts
+++ tests/md2mdast.test.ts
@@ -1,28 +1,20 @@
-import { describe, it } from "node:test";
 import fs from "node:fs";
 import path from "node:path";
 import { inspect } from "unist-util-inspect";
+import { markdown2ast } from "@/utils/blog";
 
-describe("md2mdast", () => {
-    it("list md and show as ast", async () => {
-        console.log("hell world");
-        const { default: markdown2ast } = await import(
-            // @ts-expect-error just ignore it
-            "../utils/posts/md2mdast.ts"
-        );
+test("print all markdown ast", async () => {
+    console.log("hello world");
 
-        const files = await fs.promises.readdir(
-            path.join(process.cwd(), "posts"),
-        );
+    const files = await fs.promises.readdir(path.join(process.cwd(), "posts"));
 
-        const mdFileWithoutExt = files
-            .filter((file) => file.endsWith(".md"))
-            .map((file) => file.replace(/\.md$/, ""));
+    const mdFileWithoutExt = files
+        .filter((file) => file.endsWith(".md"))
+        .map((file) => file.replace(/\.md$/, ""));
 
-        for (const file of mdFileWithoutExt) {
-            console.log("Reading file:", file);
-            const ast = await markdown2ast(file);
-            console.log(inspect(ast));
-        }
-    });
+    for (const file of mdFileWithoutExt) {
+        console.log("Reading file:", file);
+        const ast = await markdown2ast(file);
+        console.log(inspect(ast));
+    }
 });
