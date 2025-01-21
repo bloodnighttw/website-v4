98cec6df63f69522f320f14ab3c3b2271c437a2f
bloodnighttw <bbeenn1227@gmail.com>
2025-01-21 18:21:02 +0800

    feat: add snapshot to test.

--- tests/HastTableOfContent.test.tsx
+++ tests/HastTableOfContent.test.tsx
@@ -1,8 +1,8 @@
-import { test, expect } from "vitest";
-import { generateTOC } from "@/utils/posts/HastTableOfContent";
+import { expect, test } from "vitest";
+import HastTOC from "@/utils/posts/HastTableOfContent";
 import { testHAST } from "@/tests/cases/testHAST";
 
-test("HastTableOfContent | generateTOC", async () => {
-    const TOCNode = generateTOC(testHAST);
-    console.log(JSON.stringify(TOCNode, null, 4));
+test("HastTableOfContent", async () => {
+    const tocComponent = <HastTOC ast={testHAST} />;
+    expect(tocComponent).toMatchSnapshot();
 });
