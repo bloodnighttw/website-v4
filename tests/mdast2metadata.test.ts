b002d7b62d5121989a49039d27f7a61a9c717ed6
bloodnighttw <bbeenn1227@gmail.com>
2025-01-21 14:59:42 +0800

    feat: use test case and check expect output.

--- tests/mdast2metadata.test.ts
+++ tests/mdast2metadata.test.ts
@@ -1,6 +1,4 @@
-import fs from "node:fs";
-import path from "node:path";
-import { test } from "vitest";
+import { test, expect } from "vitest";
 import { markdown2ast } from "@/utils/blog";
 import {
     generateSummary,
@@ -9,46 +7,34 @@ import {
 } from "@/utils/posts/mdast2hast";
 
 test("mdast2metadata | yaml", async () => {
-    const files = await fs.promises.readdir(path.join(process.cwd(), "posts"));
+    const md = await markdown2ast("cfr2");
+    const yaml = yamlParse(md);
 
-    const mdFileWithoutExt = files
-        .filter((file) => file.endsWith(".md"))
-        .map((file) => file.replace(/\.md$/, ""));
+    const expectResult = {
+        title: "我是如何打造一個部落格系統的",
+        date: "2025-01-07",
+        categories: ["draft"],
+    };
 
-    for (const file of mdFileWithoutExt) {
-        const ast = await markdown2ast(file);
-        const node = yamlParse(ast);
-        console.log("Reading file:", file);
-        console.log(node);
-    }
+    expect(yaml).toEqual(expectResult);
 });
 
 test("mdast2metadata | summary", async () => {
-    const files = await fs.promises.readdir(path.join(process.cwd(), "posts"));
+    const expectResult =
+        "前言 我原先一直使用medium來寫文章，但畢竟還是別人的平台，是在幫別人打工，所以我決定自己來寫部落格系統。 原先我有個僅使用tailwindcss的個人網站，上面放了聯絡資訊，但是後來發現這樣的網站沒有什麼意義，所以我決定把他改成部落格，並採用下列技術。 用到的技術 Next.js 一個React的框架，他提供了很多好用的功能，像是SSR, SSG, ISR等等，讓我不太需要去擔心SEO的問題。 Tailwindcss 一個css框架，讓我可以直接用現有的design system直接實作就好了。 Markdown 我使用markdown來寫文章，這邊使用 來做到把markdown->html的部分，未來有機會可以改用WHSIWHG的編輯器與呈現方式。 Github 用於版本管理，以及區分production 和 development的分支。 Vercel 輕鬆的deploy我的部落格，最主要是免費。 Cloudflare R2 我的圖片是用Cloudflare的R2來存放的，這樣可以讓我的圖片不用放在github上，這樣可以讓我的repo比較乾淨，同時享受到Cloudflare";
 
-    const mdFileWithoutExt = files
-        .filter((file) => file.endsWith(".md"))
-        .map((file) => file.replace(/\.md$/, ""));
+    const md = await markdown2ast("cfr2");
+    const summary = generateSummary(md);
 
-    for (const file of mdFileWithoutExt) {
-        const ast = await markdown2ast(file);
-        const summary = generateSummary(ast);
-        console.log("Reading file:", file);
-        console.log(summary);
-    }
+    expect(summary).toEqual(expectResult);
 });
 
 test("mdast2metadata | thumbnail", async () => {
-    const files = await fs.promises.readdir(path.join(process.cwd(), "posts"));
+    const expectResult = "https://r2.bntw.dev/how-i-made-my-blog/r2.png";
 
-    const mdFileWithoutExt = files
-        .filter((file) => file.endsWith(".md"))
-        .map((file) => file.replace(/\.md$/, ""));
+    const md = await markdown2ast("cfr2");
 
-    for (const file of mdFileWithoutExt) {
-        const ast = await markdown2ast(file);
-        const thumbnail = generateThumbnail(ast);
-        console.log("Reading file:", file);
-        console.log(thumbnail);
-    }
+    const thumbnail = generateThumbnail(md);
+
+    expect(thumbnail).toEqual(expectResult);
 });
