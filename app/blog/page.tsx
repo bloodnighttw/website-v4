2ef34b9b27ffeeb7a3c8da0e8b9455ef60e80063
bloodnighttw <bbeenn1227@gmail.com>
2025-02-03 19:59:12 +0800

    fix: fix extra padding in blog.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,31 +16,29 @@ export default async function Blog() {
     return (
         <>
             <NavLG title="blog">
-                <div className="mx-auto mt-8 max-w-4xl px-2">
-                    {posts
-                        .sort((a, b) =>
-                            a.date < b.date ? 1 : a.date == b.date ? 0 : -1,
-                        )
-                        .map((post) => (
-                            <Link href={`/blog/${post.slug}`} key={post.slug}>
-                                <div
-                                    className="mx-auto w-full rounded-sm border border-stone-700 p-4 duration-200 hover:bg-stone-800 sm:hover:scale-105"
-                                    key={post.slug}
-                                >
-                                    <h1 className="line-clamp-1 text-3xl">
-                                        {post.title}
-                                    </h1>
-                                    <p className="pt-1 text-gray-400">
-                                        {post.date.toISOString().slice(0, 10)}
-                                    </p>
-                                    <p className="line-clamp-3 pt-2 text-gray-300">
-                                        {post.summery}
-                                    </p>
-                                </div>
-                                <br />
-                            </Link>
-                        ))}
-                </div>
+                {posts
+                    .sort((a, b) =>
+                        a.date < b.date ? 1 : a.date == b.date ? 0 : -1,
+                    )
+                    .map((post) => (
+                        <Link href={`/blog/${post.slug}`} key={post.slug}>
+                            <div
+                                className="mx-auto w-full rounded-sm border border-stone-700 p-4 duration-200 hover:bg-stone-800 sm:hover:scale-105"
+                                key={post.slug}
+                            >
+                                <h1 className="line-clamp-1 text-3xl">
+                                    {post.title}
+                                </h1>
+                                <p className="pt-1 text-gray-400">
+                                    {post.date.toISOString().slice(0, 10)}
+                                </p>
+                                <p className="line-clamp-3 pt-2 text-gray-300">
+                                    {post.summery}
+                                </p>
+                            </div>
+                            <br />
+                        </Link>
+                    ))}
             </NavLG>
         </>
     );
