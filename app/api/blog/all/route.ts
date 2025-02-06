bcd7df2f5e33aa50f17947ceda805677ad63617c
bloodnighttw <bbeenn1227@gmail.com>
2025-02-06 14:36:15 +0800

    chore: remove unused part.

--- app/api/blog/all/route.ts
+++ app/api/blog/all/route.ts
@@ -1,5 +1,3 @@
-import fs from "node:fs";
-import path from "node:path";
 import { getAllSlugs, getMDASTBySlug } from "@/utils/blog";
 import { toString } from "mdast-util-to-string";
 import { visit } from "unist-util-visit";
@@ -7,10 +5,6 @@ import { visit } from "unist-util-visit";
 export const dynamic = "force-static";
 
 export async function GET() {
-    if (!fs.existsSync(path.join(process.cwd(), "public"))) {
-        await fs.promises.mkdir(path.join(process.cwd(), "public"));
-    }
-
     const slug = await getAllSlugs();
 
     const all = slug.map(async (s) => {
