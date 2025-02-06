ff84f5d089763f4600832d1aae0504a2908f9426
bloodnighttw <bbeenn1227@gmail.com>
2025-02-06 14:46:12 +0800

    chore: make endpoint contain title.

--- app/api/blog/all/route.ts
+++ app/api/blog/all/route.ts
@@ -1,7 +1,6 @@
-import { getAllSlugs, getMDASTBySlug } from "@/utils/blog";
+import { getAllSlugs, getMDASTBySlug, getMetadata } from "@/utils/blog";
 import { toString } from "mdast-util-to-string";
 import { visit } from "unist-util-visit";
-
 export const dynamic = "force-static";
 
 export async function GET() {
@@ -9,19 +8,23 @@ export async function GET() {
 
     const all = slug.map(async (s) => {
         const mdast = await getMDASTBySlug(s);
+        const metadata = getMetadata(mdast);
 
-        visit(mdast, "yaml", (node, index, parent) => {
+        visit(mdast, "yaml", (_node, index, parent) => {
             if (parent && typeof index === "number") {
                 parent.children.splice(index, 1);
             }
         });
-
         const content = toString(mdast);
 
-        return [s, content];
+        return {
+            slug: s,
+            title: metadata.title,
+            content: content,
+        };
     });
 
     const content = await Promise.all(all);
 
-    return Response.json(Object.fromEntries(content));
+    return Response.json(content);
 }
